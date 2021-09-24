97f948a8c2c407142d5e7659394a083f7aff6819
EC2 Default User <ec2-user@ip-172-31-64-223.ec2.internal>
2021-09-24 14:31:14 +0000

    Ajax favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,14 @@
 class FavoritesController < ApplicationController
   def create
-    eatery = Eatery.find(params[:eatery_id])
-    favorite = current_user.favorites.new(eatery_id: eatery.id)
+    @eatery = Eatery.find(params[:eatery_id])
+    favorite = current_user.favorites.new(eatery_id: @eatery.id)
     favorite.save
     #eatery.create_notification_by(current_user)
-    redirect_to eatery_path(eatery)
   end
 
   def destroy
-    eatery = Eatery.find(params[:eatery_id])
-    favorite = current_user.favorites.find_by(eatery_id: eatery.id)
+    @eatery = Eatery.find(params[:eatery_id])
+    favorite = current_user.favorites.find_by(eatery_id: @eatery.id)
     favorite.destroy
-    redirect_to eatery_path(eatery)
   end
 end
